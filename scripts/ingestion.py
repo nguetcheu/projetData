2b31f65c4787af4d072bb7af0ffea5c52f79b997
nguetcheu <dnguetcheu@gmail.com>
2024-11-08 17:22:40 +0100

    formatage des dates ok

--- scripts/ingestion.py
+++ scripts/ingestion.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import date_format
 
 # Créer une session Spark
 spark = SparkSession.builder.appName("ClimateData").getOrCreate()
@@ -12,10 +13,12 @@ df_static = spark.read.csv(base_path + csv_file_path, header=True, inferSchema=T
 
 # Afficher un aperçu des données
 df_static.printSchema()
-df_static.show(10)
+#df_static.show(10)
 
 #formatage de la date en format (YYYY-MM-DD)
+df_static = df_static.withColumn("last_updated", date_format("last_updated",  "dd-MM-yyyy HH:mm"))
 
+df_static.select("last_updated").show(10)
 
 # Transfert des données vers hdfs et sauvegarde en parquet
 # df_static.write.mode("overwrite").save("hdfs://localhost:9000/user/hadoop/data/climate_data_kaggle")

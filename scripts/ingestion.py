1be2329642384ac074438ca3ac599ef877132a2d
Evrad7 <tsoataevrad7@gmail.com>
2024-11-10 16:29:54 -0500

    Formatage de la date et configuration des variables d'environnments

--- scripts/ingestion.py
+++ scripts/ingestion.py
@@ -1,19 +1,29 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, date_format
+import os
+from dotenv import load_dotenv
+
+#Chargement des variable environnements de développement
+load_dotenv(dotenv_path=".env.development")
 
 # Créer une session Spark
 spark = SparkSession.builder.appName("ClimateData").getOrCreate()
 
 # Définir le chemin de base et le chemin du fichier CSV dans des variables
-base_path = "/home/nguetcheu"
-csv_file_path = "/Documents/projetData/data/GlobalWeatherRepository.csv"
+base_path = os.environ["BASE_PATH"]
+csv_file_path = os.environ["CSV_FILE_PATH"]
+
 
 # Charger les données CSV
 df_static = spark.read.csv(base_path + csv_file_path, header=True, inferSchema=True)
 
 # Afficher un aperçu des données
-df_static.printSchema()
-df_static.show(10)
+# df_static.printSchema()
+# df_static.show(10)
+
+#format de la date
+df_static=df_static.withColumn("last_updated", date_format(col("last_updated"), "dd-MM-yyy HH:mm"))
 
 
 # Transfert des données vers hdfs
-# df_static.write.mode("overwrite").save("hdfs://localhost:9000/user/hadoop/data/climate_data_kaggle")
+df_static.write.mode("overwrite").save(os.environ["OUTPUT_FILE"])

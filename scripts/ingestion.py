72abda30f0dd9a811baf4e71bd754dd4c8711a62
evrad7 <tiojioromain02@gmail.com>
2024-11-21 09:35:32 +0100

    gestion des valeurs manquante et normalisation des textes

--- scripts/ingestion.py
+++ scripts/ingestion.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import date_format
+from pyspark.sql.functions import col, when, lower, trim
 
 
 # Créer une session Spark
@@ -8,18 +9,42 @@ spark = SparkSession.builder.appName("ClimateData").getOrCreate()
 # Définir le chemin de base et le chemin du fichier CSV dans des variables
 base_path = "/home/evrad7"
 csv_file_path = "/Documents/projetData/data/GlobalWeatherRepository.csv"
+save_path = "/home/evrad7/Documents/projetData/data/"
 
 # Charger les données CSV ,
 df_static = spark.read.csv(base_path + csv_file_path, header=True, inferSchema=True)
 
 # Afficher un aperçu des données
-df_static.printSchema()
+#df_static.printSchema()
 
 #formatage de la data en format (YYYY-MM-DD)
 df_static=df_static.withColumn("last_updated",date_format("last_updated","dd-mm-yyyy HH:mm"))
 
-df_static.select("last_updated").show(10)
+#df_static.select("last_updated").show(10)
+
+# Renommer les colonnes problématiques (remplacer '.' par '_')
+renamed_columns = [col_name.replace('.', '_') for col_name in df_static.columns]
+df_static = df_static.toDF(*renamed_columns)
+
+# Identification des colonnes numériques et textuelles
+numeric_cols = [field.name for field in df_static.schema.fields if field.dataType.simpleString() in ["int", "double", "float"]]
+text_cols = [field.name for field in df_static.schema.fields if field.dataType.simpleString() == "string"]
+
+# Remplir les valeurs manquantes
+df_filled = df_static.fillna({col: 0 for col in numeric_cols}).fillna({col: "vide" for col in text_cols})
+
+# Normaliser les colonnes de texte
+df_normalized = df_filled.select(
+    *[col(c).alias(c) for c in numeric_cols],  # Garder les colonnes numériques
+    *[lower(trim(col(c))).alias(c) for c in text_cols]  # Normaliser les colonnes textuelles
+)
+
+# Enregistrer les modifications dans le même fichier CSV
+#df_normalized.write.mode("overwrite").csv(save_path,header=True)
+
+# Afficher le résultat
+df_normalized.show()
 
 
 # Transfert des données vers hdfs
-# df_static.write.mode("overwrite").save("hdfs://localhost:9000/user/hadoop/data/climate_data_kaggle")
+#df_static.write.mode("overwrite").save("hdfs://localhost:9000/user/hadoop/Documents/climate_dat")

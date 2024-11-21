44609cfd0d4c33a69bae7bc2d7bf1e3b93ba4a33
nguetcheu <dnguetcheu@gmail.com>
2024-11-21 13:53:25 +0100

    Ajout du logging

--- scripts/ingestion.py
+++ scripts/ingestion.py
@@ -1,5 +1,9 @@
+import logging
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import date_format
+
+# Configurer le logging
+logging.basicConfig(level=logging.INFO)
 
 # Créer une session Spark
 spark = SparkSession.builder.appName("ClimateData").getOrCreate()
@@ -10,6 +14,7 @@ csv_file_path = "/Documents/projetData/data/GlobalWeatherRepository.csv"
 
 # Charger les données CSV
 df_static = spark.read.csv(base_path + csv_file_path, header=True, inferSchema=True)
+logging.info("Fichier CSV chargé avec succès.")
 
 # Afficher un aperçu des données
 df_static.printSchema()
@@ -17,6 +22,13 @@ df_static.printSchema()
 #formatage de la date en format (YYYY-MM-DD)
 df_static = df_static.withColumn("last_updated", date_format("last_updated",  "dd-MM-yyyy HH:mm"))
 
+# Renommer les colonnes problématiques (remplacer '.' par '_')
+renamed_columns = [col_name.replace('.', '_') for col_name in df_static.columns]
+df_static = df_static.toDF(*renamed_columns)
+
+# Identification des colonnes numériques et textuelles
+numeric_cols = [field.name for field in df_static.schema.fields if field.dataType.simpleString() in ["int", "double", "float"]]
+text_cols = [field.name for field in df_static.schema.fields if field.dataType.simpleString() == "string"]
 
 # Transfert des données vers hdfs et sauvegarde en parquet
 df_static.write.mode("overwrite").save("hdfs://localhost:9000/user/hadoop/data/climate_data_kaggle")
